fc2cb338e234aaa43f5d443d10e6411f94d4b9b4
Giles Anthony Villamor II <villamorbudz@gmail.com>
2023-12-14 23:56:46 +0800

    added HomePage Layout

---
+++ src/MainUI/java/com/example/capstoneapp/HomePageController.java
@@ -0,0 +1,61 @@
+package com.example.capstoneapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class HomePageController {
+
+    @FXML
+    private Button createSubjectCardButton;
+
+    @FXML
+    private Text createSubjectFolderText;
+
+    @FXML
+    private Pane createSubjectTextContainer;
+
+    @FXML
+    private Pane folderCardContainer;
+
+    @FXML
+    private Pane folderCardContainer1;
+
+    @FXML
+    private Pane headerPane;
+
+    @FXML
+    private AnchorPane homePage;
+
+    @FXML
+    private BorderPane homePageContainer;
+
+    @FXML
+    private Button subjectFolderButton;
+
+    @FXML
+    private Button subjectFolderButton1;
+
+    @FXML
+    private Text subjectFolderName;
+
+    @FXML
+    private Text subjectFolderName1;
+
+    @FXML
+    private Text subjectFolderQuizCount;
+
+    @FXML
+    private Text subjectFolderQuizCount1;
+
+    @FXML
+    private GridPane subjectFoldersGrid;
+
+    @FXML
+    private Pane subjectFoldersGridContainer;
+
+}
