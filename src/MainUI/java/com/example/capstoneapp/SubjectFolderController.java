b15bda8099de33d16d0bb63411dac1f44ea0f8d4
Giles Anthony Villamor II <villamorbudz@gmail.com>
2023-12-14 20:38:38 +0800

    added SubjectFolder UI

---
+++ src/MainUI/java/com/example/capstoneapp/SubjectFolderController.java
@@ -0,0 +1,36 @@
+package com.example.capstoneapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+public class SubjectFolderController {
+
+    @FXML
+    private TextField SubjectNameText;
+
+    @FXML
+    private Button addQuizButton;
+
+    @FXML
+    private Button exampleQuizButton;
+
+    @FXML
+    private Pane headerPane;
+
+    @FXML
+    private AnchorPane quizListContainer;
+
+    @FXML
+    private Pane quizListPane;
+
+    @FXML
+    private BorderPane subjectFolderContainer;
+
+    @FXML
+    private AnchorPane subjectFolderPane;
+
+}
